9b58e91252902e921f4e3b35d782c855157e8fd9
drishti-cs1251 <dginfo1234@gmail.com>
2026-03-15 22:20:23 +0530

    added /api to make routes work because of backend change

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
